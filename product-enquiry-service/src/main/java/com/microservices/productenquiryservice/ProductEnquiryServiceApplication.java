5b64739e79a10458b38e5ed79e2c803e2f945094
Jonasz Delimata <jonasz.dl@gmail.com>
2022-02-12 21:22:21 +0100

    Commit 8 - create Eureka Server, repair load balancer

--- product-enquiry-service/src/main/java/com/microservices/productenquiryservice/ProductEnquiryServiceApplication.java
+++ product-enquiry-service/src/main/java/com/microservices/productenquiryservice/ProductEnquiryServiceApplication.java
@@ -1,14 +1,13 @@
 package com.microservices.productenquiryservice;
 
-import com.microservices.productenquiryservice.client.LoadBalancerConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.loadbalancer.annotation.LoadBalancerClients;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableFeignClients("com.microservices.productenquiryservice")
-@LoadBalancerClients(defaultConfiguration = LoadBalancerConfiguration.class)
+@EnableDiscoveryClient
 public class ProductEnquiryServiceApplication {
 
     public static void main(String[] args) {
--- product-enquiry-service/src/main/java/com/microservices/productenquiryservice/client/ProductStockClient.java
+++ product-enquiry-service/src/main/java/com/microservices/productenquiryservice/client/ProductStockClient.java
@@ -1,16 +1,12 @@
 package com.microservices.productenquiryservice.client;
 
 import com.microservices.productenquiryservice.beans.ProductEnquiryBean;
-import org.springframework.cloud.loadbalancer.annotation.LoadBalancerClient;
-import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 //@FeignClient(name = "product-stock-service", url = "http://localhost:8200/") // without load balancer it requires url
-@FeignClient(name = "product-stock-service") // with load balancer Ribbon
-//@RibbonClient(name = "product-stock-service")
-@LoadBalancerClient(name = "product-stock-service")
+@FeignClient(name = "product-stock-service") // with Eureka server
 public interface ProductStockClient {
 
     @GetMapping("/check-product-stock/name/{productName}/availability/{productAvailability}")
